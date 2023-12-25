dc067d74cd1cd39ca9bb38c5f7d3ab81635ccc2a
Smilebek <abdurahmonovfirdavs280@gmail.com>
2023-12-25 15:08:05 +0500

    add .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import CardsCom from './components/Cards-com/Cards-com'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <CardsCom />
       <Footer />
     </>
   )
---
+++ src/components/Cards-com/Cards-com.jsx
@@ -0,0 +1,18 @@
+import "./Cards-com.scss";
+
+function CardsCom() {
+    return (
+        <div className="container">
+            <ul className="cards-list">
+                <li className="cards-item">
+                    <h2>Lorem ipsum dolor sit amet.</h2>
+                    <p></p>
+                    <img src="" alt="" />
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default CardsCom
