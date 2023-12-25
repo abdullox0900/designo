b21a2bb3ee05e4314591c2574bb51e9bbcc1cacd
JustinKosimov <jasurkosimovmj@gmail.com>
2023-12-25 14:56:08 +0500

    new pull

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,24 @@
 import "./Header.scss";
-
+import HeaderLogo from "../../assets/img/heder-logo.svg"
 function Header() {
-    return(
+    return (
         <div className="container">
-           
-           <div className="nav"></div>
+
+            <div className="header">
+                <img src={HeaderLogo} alt="" />
+                <ul className="header-list">
+                    <li className="header-item">
+                        <a className="header-link" href="#">OUR COMPANY</a>
+                    </li>
+                    <li className="header-item">
+                        <a className="header-link" href="#">LOCATIONS</a>
+                    </li>
+                    <li className="header-item">
+                        <a className="header-link" href="#">CONTACT</a>
+                    </li>
+
+                </ul>
+            </div>
         </div>
     )
 }
