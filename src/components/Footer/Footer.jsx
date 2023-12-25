a7fe841c355668db7f89bbd809a9ce923400dae2
Azimjonov08 <azimjonovm008@gmail.com>
2023-12-25 15:19:56 +0500

    add header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,62 @@
 import "./Footer.scss";
+import LogoImg from "../../assets/svg/logo.svg"
+import Faceebook from "../../assets/svg/Facebook.svg"
+import Insta from "../../assets/svg/Insta.svg"
+import Patx from "../../assets/svg/Patx.svg"
+import Twitter from "../../assets/svg/twitter.svg"
+import Youtube from "../../assets/svg/youtube.svg"
+
+
+
 
 function Footer() {
-    return(
-        
-           <footer>
-            
-           </footer>
-        
+    return (
+
+        <footer>
+            <div className="container">
+                <div className="footer-top">
+                    <img src={LogoImg} alt="logo" />
+                    <ul>
+                        <li>
+                            <a href="#">OUR COMPANY</a>
+                        </li>
+                        <li>
+                            <a href="#">LOCATIONS</a>
+                        </li>
+                        <li>
+                            <a href="#">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+                <span></span>
+
+                <div className="footer-bottom">
+                    <div className="fotter-inner">
+                        <p>
+                            Designo Central Office
+                            3886 Wellington Street
+                            Toronto, Ontario M9C 3J5
+                        </p>
+
+                        <p>
+                            Contact Us (Central Office)
+                            P : +1 253-863-8967
+                            M : contact@designo.com
+                        </p>
+
+                        <div className="images">
+                            <img src={Faceebook} alt="#" />
+                            <img src={Youtube} alt="$" />
+                            <img src={Twitter} alt="$" />
+                            <img src={Patx} alt="$" />
+                            <img src={Insta} alt="$" />
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </footer>
+
     )
 }
 
